83b11433bbef90491b05cc5333f8a00a8802c532
jcarolinares <jcarolinares@gmail.com>
2019-12-21 22:13:42 +0100

    "Free busy functionality done"

--- free-busy-office-pomodoro/free-busy-office-pomodoro/src/main.cpp
+++ free-busy-office-pomodoro/free-busy-office-pomodoro/src/main.cpp
@@ -10,6 +10,7 @@ void busy_state();
 
 // Global variables
 int state = 0;
+int acc_th = 0.4;
 /**
 States:
 0->Idle
@@ -27,7 +28,7 @@ void setup(){
   M5.begin();
   Wire.begin();
   IMU.initMPU9250(); // this line must be after Wire.begin()
-  IMU.calibrateMPU9250(IMU.gyroBias, IMU.accelBias);
+  // IMU.calibrateMPU9250(IMU.gyroBias, IMU.accelBias); //Use only if necessary on a flat surface
 
   /*
     Power chip connected to gpio21, gpio22, I2C device
@@ -50,37 +51,40 @@ if (IMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
   IMU.ay = (float)IMU.accelCount[1] * IMU.aRes; // - accelBias[1];
   // M5.Lcd.print("Y-acceleration: "); M5.Lcd.print(1000 * IMU.ay);
   // M5.Lcd.clear();
-  //   M5.Lcd.setCursor(0, 0);
-  // M5.Lcd.print("Y-acceleration: "); M5.Lcd.print(IMU.ay);
+  // M5.Lcd.setCursor(0, 0);
+  // M5.Lcd.print("Y-acceleration: ");
+  // M5.Lcd.print(IMU.ay);
 
   // M5.Lcd.println(" mg ");
 }
 
 
 // State machine transitions
-if (state != 1 && IMU.ay > 0.20) {
+if (state != 1 && IMU.ay > acc_th) {
   state = 1;
+  free_state();
 }
-else if (state != 2 && IMU.ay < -0.20) {
+else if (state != 2 && IMU.ay < -acc_th) {
   state = 2;
+  busy_state();
 }
 
 
-switch (state) {
-  case 0:
-    //It just keep the last state
-    break;
-
-  case 1:
-    free_state();
-    break;
-  case 2:
-    busy_state();
-    break;
-  default:
-    M5.Lcd.clear();
-    break;
-}
+// switch (state) {
+//   case 0:
+//     //It just keep the last state
+//     break;
+//
+//   case 1:
+//     free_state();
+//     break;
+//   case 2:
+//     busy_state();
+//     break;
+//   default:
+//     M5.Lcd.clear();
+//     break;
+// }
 }
 
 void free_state(){
@@ -94,7 +98,7 @@ void free_state(){
 void busy_state(){
   M5.Lcd.clear();
   M5.Lcd.setRotation(3);
-  M5.Lcd.setCursor(100,90);
+  M5.Lcd.setCursor(75,90);
   M5.Lcd.setTextColor(RED);
-  M5.Lcd.print("RED");
+  M5.Lcd.print("BUSY");
 }
