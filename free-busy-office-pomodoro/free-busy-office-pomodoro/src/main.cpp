578ae88fd594f121e8f37169452221a8db7670b5
jcarolinares <jcarolinares@gmail.com>
2019-12-22 13:53:44 +0100

    Basic Pomodoro timer working with button interruptions

--- free-busy-office-pomodoro/free-busy-office-pomodoro/src/main.cpp
+++ free-busy-office-pomodoro/free-busy-office-pomodoro/src/main.cpp
@@ -12,7 +12,7 @@ jcarolinares@gmail.com
 CC-BY-SA
 
 Acknowledgments and Attributions
-The LED bar control is based on the code done by G4lileo in the project
+The LED bar control is based on the code done by G4lileo on the project
 ESP32-WiFi-Hash-Monster: https://github.com/G4lile0/ESP32-WiFi-Hash-Monster
 
 **/
@@ -20,6 +20,7 @@ ESP32-WiFi-Hash-Monster: https://github.com/G4lile0/ESP32-WiFi-Hash-Monster
 #include <Arduino.h>
 #include <M5Stack.h>
 #include "utility/MPU9250.h"
+#include <Ticker.h>
 
 #ifdef ARDUINO_M5STACK_FIRE
   #include <FastLED.h>
@@ -29,13 +30,21 @@ ESP32-WiFi-Hash-Monster: https://github.com/G4lile0/ESP32-WiFi-Hash-Monster
   CRGB leds[M5STACK_FIRE_NEO_NUM_LEDS];
 #endif
 
+// The screen is 320 x 240 pixels
+#define LCD_MAX_X 320
+#define LCD_MAX_Y 240
+
+
 MPU9250 IMU; // new a MPU9250 object
 
-// The screen is 320 x 240 pixels
+Ticker tickerSetHigh;
+Ticker tickerSetLow;
+
 void free_state();
 void busy_state();
 void lunch_state();
 void pomodoro_state();
+void callback_pomodoro();
 
 // Global variables
 int acc_th = 0.4;
@@ -43,6 +52,8 @@ unsigned int bright = 100;  // default
 unsigned int bright_leds = 5;  // default
 unsigned int led_status = 0;
 unsigned int ledPacketCounter = 0;
+unsigned long pomodoro_time = 0;
+int pomodoro_minutes = 1;
 
 int state = 0;
 /**
@@ -78,12 +89,11 @@ void setup(){
   */
   M5.Power.begin();
   M5.Lcd.setTextSize(10);
-
 }
 
 // the loop routine runs over and over again forever
 void loop() {
-// M5.Lcd.fillScreen(RED);
+
 M5.update();
 
 if (IMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
@@ -113,22 +123,22 @@ else if (state != 2 && state!= 3 && state!= 4 && IMU.ay < -acc_th) {
 else if(M5.BtnA.wasPressed()){
   if(state!= 3){
     state = 3;
+    lunch_state();
   }
   else
   {
     state =0;
   }
-  lunch_state();
 }
 else if(M5.BtnB.wasPressed()){
   if(state!= 4){
     state = 4;
+    pomodoro_state();
   }
   else
   {
     state =0;
   }
-  pomodoro_state();
 }
 
 }
@@ -142,7 +152,7 @@ void free_state(){
 
   #ifdef ARDUINO_M5STACK_FIRE
     for (int pixelNumber = 0; pixelNumber < 10; pixelNumber++){
-      leds[pixelNumber].setRGB( 0, bright_leds, 0);;
+      leds[pixelNumber].setRGB( 0, bright_leds, 0);
     }
     FastLED.show();
   #endif
@@ -157,7 +167,7 @@ void busy_state(){
 
   #ifdef ARDUINO_M5STACK_FIRE
     for (int pixelNumber = 0; pixelNumber < 10; pixelNumber++){
-      leds[pixelNumber].setRGB( bright_leds, 0, 0);;
+      leds[pixelNumber].setRGB( bright_leds, 0, 0);
     }
     FastLED.show();
   #endif
@@ -172,20 +182,48 @@ void lunch_state(){
 
   #ifdef ARDUINO_M5STACK_FIRE
     for (int pixelNumber = 0; pixelNumber < 10; pixelNumber++){
-      leds[pixelNumber].setRGB( bright_leds, bright_leds, 0);;
+      leds[pixelNumber].setRGB( bright_leds, bright_leds, 0);
     }
     FastLED.show();
   #endif
 }
 
 void pomodoro_state(){
+  delay(250); //Small delay to avoid bouncing double presses in button B
+  int time_fraction = (pomodoro_minutes *60000)/LCD_MAX_Y;
+  pomodoro_time = millis();
   M5.Lcd.clear();
-  M5.Lcd.setRotation(1);
+  // M5.Lcd.setRotation(1);
   M5.Lcd.fillScreen(RED);
 
   #ifdef ARDUINO_M5STACK_FIRE
     for (int pixelNumber = 0; pixelNumber < 10; pixelNumber++){
-      leds[pixelNumber].setRGB( bright_leds, 0, 0);;
+      leds[pixelNumber].setRGB( bright_leds, 0, 0);
+    }
+    FastLED.show();
+  #endif
+
+  int i = 0;
+  while(i<LCD_MAX_Y){
+    if(millis()>=pomodoro_time+time_fraction){
+      M5.Lcd.drawFastHLine(0,i, LCD_MAX_X, TFT_BLACK);
+      i++;
+      pomodoro_time = millis();
+    }
+    else if(M5.BtnA.wasPressed() || M5.BtnB.read() || M5.BtnC.wasPressed()){
+      break;
+    }
+    M5.update();
+  }
+  M5.Lcd.clear();
+  // M5.Lcd.setRotation(1);
+  M5.Lcd.setCursor(60,90);
+  M5.Lcd.setTextColor(GREEN);
+  M5.Lcd.print("BREAK");
+
+  #ifdef ARDUINO_M5STACK_FIRE
+    for (int pixelNumber = 0; pixelNumber < 10; pixelNumber++){
+      leds[pixelNumber].setRGB(0, bright_leds, 0);
     }
     FastLED.show();
   #endif
